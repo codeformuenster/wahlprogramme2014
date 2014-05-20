50a3e77bca67be2845572f0f0402cec36284f8b3
Tobias Bradtke <webwurst@gmail.com>
2014-05-21 01:55:17 +0200

    Add explicit mapping to index 'agenda_with_chapters'

--- parser.rb
+++ parser.rb
@@ -9,6 +9,37 @@ client = Elasticsearch::Client.new hosts: [
   { host: ENV['ELASTICSEARCH_1_PORT_9200_TCP_ADDR'] || 'localhost', port: ENV['ELASTICSEARCH_1_PORT_9200_TCP_PORT'] || 9200 }
 ]
 
+puts "Deleting index #{INDEX_NAME} from Database"
+client.indices.delete index: INDEX_NAME
+
+# https://github.com/elasticsearch/elasticsearch-ruby/blob/master/elasticsearch-api/lib/elasticsearch/api/actions/indices/create.rb#L10
+# http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/mapping-core-types.html
+# http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/index-modules-similarity.html
+# http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/analysis-lang-analyzer.html
+
+client.indices.create index: INDEX_NAME, body: {
+  mappings: {
+    chapter: {
+      properties: {
+        party: {
+          type: 'string',
+          index: 'not_analyzed'
+        },
+        position: {
+          type: 'integer'
+        },
+        text: {
+          type: 'string',
+          analyzer: 'german'
+        },
+        title: {
+          type: 'string'
+        }
+      }
+    }
+  }
+}
+
 id = 1
 Dir.foreach(CONVERTED_DIR) do |file|
   next if file[0] == '.'
@@ -27,11 +58,6 @@ Dir.foreach(CONVERTED_DIR) do |file|
     else
       text << line
     end
-  end
-
-  if client.indices.exists index: INDEX_NAME
-    puts "Deleting all #{party} chapters from Elasticsearch"
-    client.delete_by_query index: INDEX_NAME, type: 'chapter', q: "party:#{party}"
   end
 
   puts "Adding #{chapters.size} #{party} chapters to Elasticsearch"
